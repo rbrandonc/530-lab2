5b311e21a7e19112b89f0182d7936fb7bf14eb10
Brandon <r.brandon.c@gmail.com>
2016-10-25 23:28:03 -0400

    second commit

---
+++ test.c
@@ -0,0 +1,159 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+
+// //printf("test");
+
+// int i;
+//   int r = 0;
+
+// while(r < 15){
+// 	  int free_objects = 1;
+
+//   for(i = 0; i <= r; i++){
+//     free_objects = free_objects * 2;
+//   }
+
+//   printf("2^%d: %d\n", i, free_objects);
+//   r++;
+// }
+
+  
+
+
+
+
+
+
+
+
+
+
+void *x = malloc(64);
+
+
+
+
+free(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//test 10
+	// int *x = malloc(2048);
+	// int *c = malloc(2048);
+	// int *v = malloc(2048);
+	// int *b = malloc(2048);
+
+	// free(x); free(c); free(v); free(b);
+
+
+	//test4
+	// void *x;
+	// for(int i = 0; i < 128; i++){
+	// 	x = malloc(32);
+	// }
+
+	// for(int i = 0; i < 128; i++){
+	// 	void *x = malloc(32);
+	// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //printf("malloc");
+
+	//test3
+	// void *x = malloc(32);
+	// free(x);
+	// x = malloc(64);
+	// free(x);
+	// x = malloc(128);
+	// free(x);
+	// x = malloc(256);
+	// free(x);
+	// x = malloc(512);
+	// free(x);
+	// x = malloc(1024);
+	// free(x);
+	// x = malloc(2048);
+	// free(x);
+
+
+
+
+//test 1
+// 	int num = 500;
+// srand(time(NULL));
+// int i;
+// 	for(i =0; i < num; i++){
+// 		int r = 513;
+// 		printf("%d\n", r);
+// 		void *x = malloc(r);
+// 		//printf("test");
+//   		printf("%d: %p\n",i, x);
+// 	}
+
+		//test2
+		// void *x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+		// x = malloc(2048);
+		// free(x);
+
+  return (errno);
+}
